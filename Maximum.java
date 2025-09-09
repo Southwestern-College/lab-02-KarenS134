a90eb8fe24c1e273656fae98352e6bb58a9bf6b9
KarenS134 <ks1212735@swccd.edu>
2025-09-09 20:09:03 +0000

    Updated

--- Maximum.java
+++ Maximum.java
@@ -1,7 +1,9 @@
+
 // Karen Sandoval and Julius Renteria //
 import java.util.Scanner;
+
 public class Maximum {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter 3 numbers to find the max!");
         System.out.println("Enter first number:");
@@ -11,24 +13,23 @@ public class Maximum {
         System.out.println("Enter the last number:");
         int number3 = input.nextInt();
         int max;
-        //User's inputs //
+        // User's inputs //
 
-        if(number1 >= number2) {
+        if (number1 >= number2) {
             max = number1;
-        }
-        else max = number2;
-
-                if(number2 >= number3) {
+        } else
             max = number2;
-        }
-        else max = number3;
 
-                if(number1 >= number3) {
-            max = number1;
-        }
-        else
+        if (number2 >= number3) {
+            max = number2;
+        } else
             max = number3;
-        System.out.println("This is the max of the numbers you entered:"+ max);
-        //Max output //
+
+        if (number1 >= number3) {
+            max = number1;
+        } else
+            max = number3;
+        System.out.println("This is the max of the numbers you entered:" + max);
+        // Max output //
     }
 }
