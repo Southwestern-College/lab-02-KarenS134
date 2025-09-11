c66932490be4a3305d892367478eb88eca8b9c1a
KarenS134 <ks1212735@swccd.edu>
2025-09-11 20:10:09 +0000

    Updated

--- Maximum.java
+++ Maximum.java
@@ -1,4 +1,8 @@
-/*@Karen Sandoval */
+
+/**
+ * @author Karen Sandoval
+ * Displays the maximum of a set of three numbers input by the user
+ */
 // Karen Sandoval and Julius Renteria //
 import java.util.Scanner;
 
@@ -12,10 +16,17 @@ public class Maximum {
         int number2 = input.nextInt();
         System.out.println("Enter the last number:");
         int number3 = input.nextInt();
-        int max;
+        
+        int max = number1;
+        if (number2 > max) {
+            max = number2;
+        }
+        if (number3 > max) {
+            max = number3;
+        }
         // User's inputs //
 
-        if (number1 >= number2) {
+        /*if (number1 >= number2) {
             max = number1;
         } else
             max = number2;
@@ -28,7 +39,10 @@ public class Maximum {
         if (number2 >= number3) {
             max = number2;
         } else
-            max = number3;
+            max = number3;*/
+
+        
+        
 
 
         System.out.println("This is the max of the numbers you entered:" + max);
